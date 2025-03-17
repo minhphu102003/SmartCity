e546fb0e229d876388b5117908999f7d7f25529b
phuctien1909 <nguyenhoangphuctien190903@gmail.com>
2025-03-17 20:25:50 +0700

    create detail user profile page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,11 +1,13 @@
 import { LayoutAuth } from '../layouts/index';
 import { Home, LogIn, Register } from '../pages/index';
 import { PATHS } from '../constants';
+import MyProfile from '../pages/myPage/MyProfile';
 
 const router = [
     { path: PATHS.HOME, component: Home },
     { path: PATHS.LOGIN, layout: LayoutAuth, component: LogIn },
     { path: PATHS.REGISTER, layout: LayoutAuth, component: Register },
+    { path: PATHS.PROFILE, component: MyProfile },
 ];
 
 export { router };
