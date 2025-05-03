c948a3e629d3db8bbab09c05bf32fb413b09b39d
Tobi <124958881+minhphu102003@users.noreply.github.com>
2025-05-03 22:21:17 +0700

    f/feat: add map in dashboard (#43)

--- frontend/src/constants/endPoints.js
+++ frontend/src/constants/endPoints.js
@@ -17,3 +17,4 @@ export const PLACE_AUTO_COMPLETE_ENDPOINT = '/Place/AutoComplete';
 export const PLACE_DETAIL_ENDPOINT = "/Place/Detail";
 export const REPORT_ENDPOINT = '/account-report';
 export const NOTIFICATION_ENDPOINT = '/notification';
+export const CAMERA_REPORT_ENDPOINT= '/camera-report';
